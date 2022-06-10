b93e2b390cefd681f49191ff5b86c7a538deaa12
ggzzggzz <ggzz553@gmail.com>
2022-06-10 18:09:24 +0900

    pch

---
+++ src/main/java/com/team/honeybee/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.team.honeybee.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin")
+public class AdminController {
+	
+	@RequestMapping("index")
+	public void index() {
+		
+	}
+	
+}
